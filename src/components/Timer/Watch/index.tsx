772878e32291284d8375153ea0c5260637382987
Jessica Andrade <andradejaf@outlook.com>
2023-11-08 09:48:27 +0000

    add timer funcionality

--- src/components/Timer/Watch/index.tsx
+++ src/components/Timer/Watch/index.tsx
@@ -1,13 +1,22 @@
 import style from "./watch.module.scss";
 
-const Watch = () => {
+interface Props {
+  time: number | undefined;
+}
+
+const Watch = ({ time = 0 }: Props) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const [tenMinute, unitMinute] = String(minutes).padStart(2, "0");
+  const [tenSecond, unitSecond] = String(seconds).padStart(2, "0");
+
   return (
     <>
-      <span className={style.watchNumber}>0</span>
-      <span className={style.watchNumber}>0</span>
+      <span className={style.watchNumber}>{tenMinute}</span>
+      <span className={style.watchNumber}>{unitMinute}</span>
       <span className={style.watchDivision}>:</span>
-      <span className={style.watchNumber}>0</span>
-      <span className={style.watchNumber}>0</span>
+      <span className={style.watchNumber}>{tenSecond}</span>
+      <span className={style.watchNumber}>{unitSecond}</span>
     </>
   );
 };
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import timeToSeconds from "../../common/utils/time";
 import ITask from "../../types/ITask";
 import Button from "../Button";
@@ -12,16 +12,17 @@ interface Props {
 const Timer = ({ selected }: Props) => {
   const [time, setTime] = useState<number>();
 
-  if (selected?.time) {
-    setTime(timeToSeconds(selected.time));
-  }
+  useEffect(() => {
+    if (selected?.time) {
+      setTime(timeToSeconds(selected.time));
+    }
+  }, [selected]);
 
   return (
     <div className={style.timer}>
       <p className={style.title}>Choose a card and start the timer</p>
-      Time: {time}
       <div className={style.watchWrapper}>
-        <Watch />
+        <Watch time={time} />
       </div>
       <Button>Start!</Button>
     </div>
